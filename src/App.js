5f570b2cd0013e41ae8b896941bf6af0efbfdc37
MahamShakir <maham.shakir6@gmail.com>
2021-04-19 16:06:09 +0500

    Packages Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useEffect, useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import FindInPage from '@material-ui/icons/FindInPage';
+import Search from '@material-ui/icons/Search'
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import 'fontsource-roboto';
+var pluralize = require('pluralize')
+
 
 function App() {
+  let [input, setInput] = useState("");
+  let [result, setResult] = useState("NaN");
+
+  function handleInput(e){
+    setInput(e.target.value);
+  }
+
+  function handleClick(){
+    alert(input);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container spacing={3} alignItems="center" justify="center" style={{margin:"auto"}}>
+        <Typography variant="h3" style={{textAlign:"center" , color:"#3f51b5" , marginTop: "20%" }} >Vector Space Model</Typography>
+        <div style={{ marginTop:10 ,height:"1px" , width:"100%", backgroundColor:"gray"}}>
+        </div>
+        <Grid item xs={12} >
+        </Grid>
+        <Grid item xs={12} >
+        </Grid>
+        <Grid item  >
+          <FindInPage/>
+        </Grid>
+        <Grid item alignItems="center" justify="center" xs={8}>
+          <TextField id="search-field" label="Enter Query" fullWidth="true" onChange={handleInput} onKeyPress={(e) => { if(e.key === 'Enter') handleClick()}} />
+        </Grid>
+        <Grid item xs={12} >
+        </Grid>
+        <Button
+        onClick={handleClick}
+        variant="contained"
+        color="primary"
+        size="medium"
+        startIcon={<Search />}>
+        Search
+        </Button>
+        <div style={{ marginTop:20 , height:"1px" , width:"100%", backgroundColor:"gray"}}>
+        </div>
+
+        <Grid item xs={11} style={{marginTop:15}}>
+          <Typography variant="button" style={{color:"#3f51b5", fontWeight:"bold"}}>Result Set:</Typography>
+          <Card variant="outlined" style={{backgroundColor:"#e0e0e6"}}>
+            <CardContent style={{overflowWrap:"break-word"}}>
+              <Typography variant="button" style={{fontSize:"18px"}} >{result}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
